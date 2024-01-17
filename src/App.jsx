b52cc547b17d0aaa4ae0a68c99d5b1c5a0dc752c
Louise Elliott <louise.elliott.work@gmail.com>
2024-01-17 22:44:10 +0000

    react router paths set up with placeholder pages and files and unstyled navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
+import Home from "./pages/Home.jsx";
+import MeetTheTeam from "./pages/MeetTheTeam.jsx";
+import Contact from "./pages/Contact.jsx";
 
+// Top-level component, rendering all child components, with Home set as default page.
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="text-3xl font-bold underline">
+      <Router>
+         {/* Header visible across all pages */}
+        <NavBar />
+        <Routes>
+          {/* Different paths set to be rendered, with Home set as the default */}
+          <Route path="/" element={<Home />} />
+          <Route path="meetTheTeam" element={<MeetTheTeam />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
-

f37b11f0629e9e61338bc9ce65acaa43ee9708f0
Greenie115 <dggreen545@gmail.com>
2024-01-18 12:23:46 +0000

    added prebuild modal to work from

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Example from "./components/WelcomeModal";
 
 // Home page.
 function Home() {
     return (
         <div>
+            <Example />
             <h1>Home placeholder</h1>
         </div>
     )

20b8b80531b205aeff9889bbc689db7b9783629b
Chae-Lee <katelee1129@gmail.com>
2024-01-20 20:28:28 +0000

    location pin on google maps using longitude and latitude added - static version

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserLocation from "../UserLocation";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import "../michelin.json";
 
-function Maps() {}
+function Maps() {
+  useEffect(() => {
+    const iframeData = document.getElementById("iframeId");
+    const latitude = 38.921295;
+    const longitude = -77.043915;
+    iframeData.src = `https://maps.google.com/maps?q=${latitude}, ${longitude}&h1=es;&output=embed`;
+  });
+  return (
+    <div>
+      <iframe id="iframeId" height="500px" width="100%"></iframe>
+    </div>
+  );
+}
 
 export default Maps;

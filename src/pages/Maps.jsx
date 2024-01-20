931006733a028341ed45cdb3e122a6fc928a33ad
Chae-Lee <katelee1129@gmail.com>
2024-01-20 18:52:32 +0000

    google maps api

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,4 +1,7 @@
 import React from "react";
 import UserLocation from "../UserLocation";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+function Maps() {}
 
 export default Maps;

28395c5fb88ed977a81f7f90d5926343acf9254a
Chae-Lee <katelee1129@gmail.com>
2024-01-21 14:25:48 +0000

    resolving map bug

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from "react";
 import userLocation from "../UserLocation";
 import michelinData from "../michelin.json";
 
-function Maps({ restaurantData, userLocation }) {
+function Maps({ michelinData, userLocation }) {
   useEffect(() => {
+    michelinData.map((michelinData) => {
+      const restaurantLongitude = michelinData.Longitude;
+      const restaurantLatitude = michelinData.Latitude;
+      let { Longitude, Latitude } = michelinData;
+      console.log(Longitude, Latitude);
+    });
+
     const iframeData = document.getElementById("iframeId");
 
-    for (let i = 0; i < restaurantData.length; i++) {
-      const { Latitude, Longitude } = restaurantData[i];
+    for (let i = 0; i < michelinData.length; i++) {
+      const { Latitude, Longitude } = michelinData[i];
       iframeData.src = `https://maps.google.com/maps?q=${Latitude}, ${Longitude}&h1=es;&output=embed`;
     }
-  }, [restaurantData]);
-
-  // if (restaurantData && restaurantData.length > 0) {
-  //   const iframeData = document.getElementById("iframeId");
-  //   const markers = restaurantData.map((restaurant) => {
-  //     return `${restaurant.Latitude}, ${restaurant.Longitude}`;
-  //   });
-  // }
-  // const { Latitude, Longitude } = restaurantData;
-
-  // const latitude = 38.921295;
-  // const longitude = -77.043915;
+  }, [michelinData]);
 
   return (
     <div>
@@ -31,3 +27,14 @@ function Maps({ restaurantData, userLocation }) {
 }
 
 export default Maps;
+
+// if (restaurantData && restaurantData.length > 0) {
+//   const iframeData = document.getElementById("iframeId");
+//   const markers = restaurantData.map((restaurant) => {
+//     return `${restaurant.Latitude}, ${restaurant.Longitude}`;
+//   });
+// }
+// const { Latitude, Longitude } = restaurantData;
+
+// const latitude = 38.921295;
+// const longitude = -77.043915;

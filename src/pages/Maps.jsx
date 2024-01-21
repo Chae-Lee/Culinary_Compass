95b733dde5c0d6a057135fd3342ea173f6bd79e4
Chae-Lee <katelee1129@gmail.com>
2024-01-21 15:36:38 +0000

    code to iterate through all objects within the michelin.json

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -3,21 +3,32 @@ import UserLocation from "../UserLocation";
 import michelinData from "../michelin.json";
 
 function Maps({ restaurantData }) {
+  //fetching lng/lat data from michelin.json file
   michelinData.map((michelinData) => {
     const restaurantLongitude = michelinData.Longitude;
     const restaurantLatitude = michelinData.Latitude;
+    const restaurantLocation = [restaurantLongitude, restaurantLatitude];
     let { Longitude, Latitude } = michelinData;
-    console.log(Longitude, Latitude);
+    // console.log(Longitude, Latitude);
   });
 
+  // iterating through the michelin.json data and displaying on the map
   useEffect(() => {
     const iframeData = document.getElementById("iframeId");
 
-    for (let i = 0; i < restaurantData.length; i++) {
-      const { Latitude, Longitude } = restaurantData[i];
-      iframeData.src = `https://maps.google.com/maps?q=${Latitude}, ${Longitude}&h1=es;&output=embed`;
-    }
-  }, [restaurantData]);
+    michelinData.forEach(function (restaurant) {
+      console.log(
+        `this is the Longitude: ${restaurant.Longitude}, Latitude ${restaurant.Latitude}`
+      );
+      iframeData.src = `https://maps.google.com/maps?q=${restaurant.Latitude}, ${restaurant.Longitude}&h1=es;&output=embed`;
+    });
+
+    // for (let i = 0; i < michelinData.length; i++) {
+    //   const { Latitude, Longitude } = michelinData[i];
+    //   iframeData.src = `https://maps.google.com/maps?q=${Latitude}, ${Longitude}&h1=es;&output=embed`;
+    // console.log(Longitude);
+    // }
+  }, [michelinData]);
 
   return (
     <div>

97e815efbeed16858d0eb2764dea96e07d8f5394
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-01-21 09:16:18 +0000

    add scroll to MultiResults, tidy up code

--- src/pages/components/MultiResults.jsx
+++ src/pages/components/MultiResults.jsx
@@ -68,27 +68,30 @@ const testData = [
 
 export default function MultiResults() {
     return (
+      <div className="max-h-[750px] overflow-y-auto">
+        {/* Creates a UL for all results */}
         <ul role="list" className="divide-y divide-gray-100">
-                        {testData.map((restaurant) => (
-                            <li key={restaurant.name} className="flex justify-between gap-x-6 py-5">
-                            <div className="flex min-w-0 gap-x-4">
-                                
-                                <img className="h-24 w-24 flex-none rounded-md bg-gray-50" src={restaurant.imageUrl} alt="" />
-                                
-                                <div className="min-w-0 flex-auto">
-                                <p className="text-sm font-semibold leading-6 text-gray-900">{restaurant.name}</p>
-                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{restaurant.Cuisine}</p>
-                                
-                                <img className="h-4 mt-1.5 mb-1.5" src={restaurant.Award} alt="" />
-                                <a className="mt-1 truncate text-xs leading-5 text-gray-500 underline" href ={restaurant.Url}>Read the Michelin Guide</a>
-                                </div>
-                            </div>
-                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                <p className="text-sm leading-6 text-gray-900">{restaurant.Location}</p>
-                                <p className="text-xs leading-5 text-gray-500">{restaurant.Country}</p>
-                            </div>
-                            </li>
-                        ))}
-                    </ul>
+            {testData.map((restaurant) => (
+              <li key={restaurant.name} className="flex justify-between gap-x-6 py-5">
+                <div className="flex min-w-0 gap-x-4">
+                {/* CuisineImage */}
+                <img className="h-24 w-24 flex-none rounded-md bg-gray-50" src={restaurant.imageUrl} alt="" />      
+                  {/* Restaurant Info */}
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{restaurant.name}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{restaurant.Cuisine}</p>           
+                    <img className="h-4 mt-1.5 mb-1.5" src={restaurant.Award} alt="" />
+                    <a className="mt-1 truncate text-xs leading-5 text-gray-500 underline" href ={restaurant.Url}>Read the Michelin Guide</a>
+                  </div>
+                </div>
+                {/* Restaurant Location */}
+                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end pr-4">
+                  <p className="text-sm leading-6 text-gray-900">{restaurant.Location}</p>
+                  <p className="text-xs leading-5 text-gray-500">{restaurant.Country}</p>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
     )
 }

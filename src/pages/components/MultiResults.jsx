7efc66511b15289be99dc6acd68392ccbc8bbb53
Greenie115 <dggreen545@gmail.com>
2024-01-24 20:21:05 +0000

    work in progres for filtering results

--- src/pages/components/MultiResults.jsx
+++ src/pages/components/MultiResults.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import restaurants from "/src/michelin.json";
 import LoadingIcon from "./LoadingIcon";
+import michelin from "../../michelin.json"
 
-export default function MultiResults({toggleThreeStars, filteredResults}) {
+export default function MultiResults({ filteredResults }) {
 
-  const [threeStarToggle, setThreeStarToggle] = useState(true)
+console.log(filteredResults)
 
-  console.log(filteredResults)
-
-  //! Test value only
-  let testCountry = "Italy"
-
-  
   // Control whether the data is loading or not
   const [isDataLoading, setIsDataLoading] = useState(true);
 
@@ -36,37 +31,38 @@ export default function MultiResults({toggleThreeStars, filteredResults}) {
       <div className="max-h-[750px] overflow-y-auto">
         {/* Creates a UL for all results */}
         <ul role="list" className="divide-y divide-gray-100">
-          {filteredResults?.map((result) => (
-            <li key={result.ID}>
-              {result.Award}
+          {filteredResultsArray.map((result) => (
+            <li key={result.ID} className="flex justify-between gap-x-6 py-5">
+              <div className="flex min-w-0 gap-x-4">
+                {/* CuisineImage */}
+                <img className="h-24 w-24 flex-none rounded-md bg-gray-50 object-cover" src={result.CuisineImage} alt="" />
+                {/* Restaurant Info */}
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{result.Name}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{result.Cuisine}</p>
+                  <img className="h-4 mt-1.5 mb-1.5" src={result.AwardIcon} alt="" />
+                  <a className="mt-1 truncate text-xs leading-5 text-gray-500 underline" href={result.Url}>Read the Michelin Guide</a>
+                </div>
+              </div>
+              {/* Restaurant Location */}
+              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end pr-4">
+                <p className="text-sm leading-6 text-gray-900">{result.Location}</p>
+                <p className="text-xs leading-5 text-gray-500">{result.Country}</p>
+              </div>
             </li>
-            
-          
-          ))} 
-          </ul>
-        </div> 
+          ))}
+        </ul>
+      </div>
     )
-}}
+  }
+}
+
+// {
+//   filteredResults?.map((result) => (
+//     <li key={result.ID}>
+//       {result.Award}
+//     </li>
 
 
-
-// restaurants.filter((restaurant) => restaurant.Country = testCountry).map((restaurants) => (
-//   <li key={restaurants.ID} className="flex justify-between gap-x-6 py-5">
-//     <div className="flex min-w-0 gap-x-4">
-//     {/* CuisineImage */}
-//     <img className="h-24 w-24 flex-none rounded-md bg-gray-50 object-cover" src={restaurants.CuisineImage} alt="" />      
-//       {/* Restaurant Info */}
-//       <div className="min-w-0 flex-auto">
-//         <p className="text-sm font-semibold leading-6 text-gray-900">{restaurants.Name}</p>
-//         <p className="mt-1 truncate text-xs leading-5 text-gray-500">{restaurants.Cuisine}</p>           
-//         <img className="h-4 mt-1.5 mb-1.5" src={restaurants.AwardIcon} alt="" />
-//         <a className="mt-1 truncate text-xs leading-5 text-gray-500 underline" href ={restaurants.Url}>Read the Michelin Guide</a>
-//       </div>
-//     </div>
-//     {/* Restaurant Location */}
-//     <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end pr-4">
-//       <p className="text-sm leading-6 text-gray-900">{restaurants.Location}</p>
-//       <p className="text-xs leading-5 text-gray-500">{restaurants.Country}</p>
-//     </div>
-//   </li>
-// ))
+//   ))
+// }
--- src/utils/starFilters.js
+++ src/utils/starFilters.js
@@ -2,6 +2,9 @@ import michelin from '../michelin.json' assert {type: 'json'}
 
 function resultFilter(key, value){
     const result = michelin.filter(entry => entry[key] == value)
+    if(key || value == undefined){
+        return michelin
+    }
     return result
 }
 
